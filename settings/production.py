fde15b7d58be2072f3c915e94db30c860140f3a9
jMyles <justin@justinholmes.com>
2014-06-23 10:57:19 -0400

    Fixing settings structure a bit.

---
+++ settings/production.py
@@ -0,0 +1,25 @@
+from settings.secrets import DATABASE_PASSWORD
+
+from common import *
+
+DEBUG = True
+DEV_SERVER = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "kayemyles_db",
+        "USER": "kayemyles",
+        "PASSWORD": DATABASE_PASSWORD,
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+
+try:
+    from mezzanine.utils.conf import set_dynamic_settings
+except ImportError:
+    pass
+else:
+    set_dynamic_settings(globals())
