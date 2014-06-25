c4ba68853200364798faf9af250ccb93a433793d
jMyles <justin@justinholmes.com>
2014-06-25 17:52:19 -0400

    Fixed bug that caused templates to be missing.
    Some decent style upgrades.

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ for i, arg in enumerate(sys.argv):
 
 # Run Django.
 if __name__ == "__main__":
-    settings_module = "%s.settings" % PROJECT_DIRNAME
+    settings_module = "settings.local"
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
