eb3836423d63bf157428560a2cc231f8ae71f8a2
ckaye89 <chelseakaye89@gmail.com>
2015-04-25 12:41:28 -0400

    Began creating visit model.

--- apps/places/models.py
+++ apps/places/models.py
@@ -4,10 +4,21 @@ from mezzanine.pages.models import Page
 
 class Place(Page):
     '''
-    Places we have existed in.
+    Places in which we have existed.
     '''
 
     within = models.ForeignKey("places.Place", related_name="contains", blank=True, null=True)
 
     def contains_places(self):
         return self.contains.exists()
+
+
+class Visit(models.Model):
+    '''
+    Visits are the duration in which we are in a place.
+    '''
+
+    place = models.ForeignKey("places.Place", related_name="visits")
+    arrival = models.DateTimeField(blank=True, null=True)
+    departure = models.DateTimeField(blank=True, null=True)
+    visitors = 
