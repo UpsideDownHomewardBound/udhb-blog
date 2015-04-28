dd672d8e4d405bd46786f9c6ceb64469605dea86
ckaye89 <chelseakaye89@gmail.com>
2015-04-28 18:49:55 -0400

    Continued writing the places app test. Another day at Gimme Coffee.

--- apps/places/models.py
+++ apps/places/models.py
@@ -3,23 +3,25 @@ from django.conf import settings
 from mezzanine.pages.models import Page
 
 
-class Place(Page):
+class Place(models.Model):
     '''
     Places in which we have existed.
     '''
 
-    name = models.CharField()
+    name = models.CharField(max_length=50)
     within = models.ForeignKey("places.Place", related_name="contains", blank=True, null=True)
 
     def contains_places(self):
         return self.contains.exists()
+
+    def users_who_have_visited(self):
+        return  # TODO
 
 
 class VisitManager(models.Manager):
 
     def completely_new(self, stuff):
         pass
-
 
 
 class Visit(models.Model):
@@ -31,37 +33,47 @@ class Visit(models.Model):
 
     place = models.ForeignKey("places.Place", related_name="visits")
 
+    def end(self):
+        '''
+        The latest datetime of departure for a Visitor.
 
-class VisitBookEnd(models.Model):
+        If any Visitors have not yet departed, returns None.
+        '''
+        pass
 
-    class Meta:
-        abstract = True
 
+class Arrival(models.Model):
     visit = models.ForeignKey("places.Visit")
     datetime = models.DateTimeField()
 
 
-class Arrival(VisitBookEnd):
-    pass
-
-
-class Departure(VisitBookEnd):
-    pass
+class Departure(models.Model):
+    concludes = models.ForeignKey(Arrival)
+    datetime = models.DateTimeField()
 
 
 class Visitor(models.Model):
+    '''
+    A person engaging in a visit.
+
+    If the same person leaves a visit and returns to it, we will
+    make a new Visitor instance.
+    '''
     person = models.ForeignKey(settings.AUTH_USER_MODEL)
     arrival = models.ForeignKey(Arrival)
     departure = models.ForeignKey(Departure)
 
+    def is_first_appearance(self):
+        pass
+
 
 class TravelMode(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     description = models.TextField()
 
 
 class Journey(models.Model):
-    beginning = models.ForeignKey(Visit)
-    ending = models.ForeignKey(Visit)
+    beginning = models.ForeignKey(Visit, related_name="launchings")
+    ending = models.ForeignKey(Visit, related_name="landings")
     mode = models.ForeignKey(TravelMode)
     description = models.TextField()
--- apps/places/tests.py
+++ apps/places/tests.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib.auth.models import User
 from django.test import TestCase
 from apps.places.models import Place
 
@@ -12,15 +13,15 @@ class NewVisitTests(TestCase):
 
         scotland = Place.objects.create(name="Scotland")
 
-        joe = User
-        jane = User
+        joe = User.objects.create(username="joe")
+        jane = User.objects.create(username="jane")
 
-        self.assertFalse(joe.has_been_to, scotland)
+        self.assertIn(scotland.all_visitors, joe)
 
         Visit.objects.completely_new(
-            visitor = [joe, jane]
-            arrival = datetime.datetime.now()
-            place = some_place
+            visitor=[joe, jane],
+            arrival=datetime.datetime.now(),
+            place=some_place
         )
 
         self.assertTrue(joe.has_been_to, scotland)
