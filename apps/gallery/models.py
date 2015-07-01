a78654f8f5d2b918d95902f36932b0ec3b2fdbde
ckaye89 <chelseakaye89@gmail.com>
2015-06-30 21:25:43 -0400

    Began gallery model

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,18 +1,15 @@
 class Image(models.Model):
-
     name = models.CharField(max_length=200)
     full = models.FileField()
     thumb = models.FileField()
 
 class Album(models.Model):
-
     name = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField()
     slug = models.SlugField()
 
 class ImagePlacementInAlbum(models.Model):
-    
     image = models.ForeignKey(Image)
     album = models.ForeignKey(Album)
     caption = models.CharField(max_length=200)
