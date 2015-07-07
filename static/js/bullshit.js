3645416f1a9539cc0d5d011557aff59908e0da63
jMyles <justin@justinholmes.com>
2015-07-07 19:26:26 -0400

    More gallery styling.

--- static/js/bullshit.js
+++ static/js/bullshit.js
@@ -1,12 +1,3 @@
-{% if page.theme %}
-heroFontSize = {{page.theme.font_size}}
-{% elif blog_post.theme %}
-heroFontSize = {{blog_post.theme.font_size}}
-{% else %}
-heroFontSize = 700
-{% endif %}
-
-
 function slowDecorate() {
     var timeout = 400,
     time_per_block = 550;
@@ -24,6 +15,39 @@ function slowDecorate() {
     });
 }
 
+function bannerSize(startingSize) {
+      var scrollTop = $(window).scrollTop();
+      scrollRemaining = startingSize - scrollTop;
+      size = (scrollRemaining / 5) - 10;
+      if (size < 1) {
+        size = 0  // Some browsers don't like very small or negative font sizes.
+      }
+      $('#banner-title').css('font-size', size + "px");
+    }
+
+
+function textColor() {
+  var scrollTop = $(window).scrollTop();
+  var totalHeight = document.body.scrollHeight;
+
+  if (scrollTop < (totalHeight / 2) - 200) {
+    $('#page-wrapper').css('color', '#FCAC5B').css('text-shadow', '1px 1px 1px black');
+  } else {
+    $('#page-wrapper').css('color', '#4F2800').css('text-shadow', '0px 0px 1px #86969C');
+  }
+
+  if (scrollTop < (totalHeight / 2) - 200) {
+    $('#page-wrapper a').css('color', '#C7BFBF');
+  } else {
+    $('#page-wrapper a').css('color', '#6166FF');
+  }
+  if (scrollTop < (totalHeight / 2) - 200) {
+    $('h2, h3, h4, h5, h6, .h2, .h3, .h4, .h5, .h6').css('color', '#C7BFBF');
+  } else {
+    $('h2, h3, h4, h5, h6, .h2, .h3, .h4, .h5, .h6').css('color', '#593E22');
+  }
+}
+
 
   $(document).ready(function() {
   bannerSize();
@@ -37,40 +61,11 @@ function slowDecorate() {
         }
     );
 
-    function bannerSize(startingSize) {
-      var scrollTop = $(window).scrollTop();
-      scrollRemaining = startingSize - scrollTop;
-      size = (scrollRemaining / 5) - 10;
-      if (size < 1) {
-        size = 0  // Some browsers don't like very small or negative font sizes.
-      }
-      $('#banner-title').css('font-size', size + "px");
-    }
-
-    function textColor() {
-      var scrollTop = $(window).scrollTop();
-      var totalHeight = document.body.scrollHeight;
-
-      if (scrollTop < (totalHeight / 2) - 200) {
-        $('#page-wrapper').css('color', '#FCAC5B').css('text-shadow', '1px 1px 1px black');
-      } else {
-        $('#page-wrapper').css('color', '#4F2800').css('text-shadow', '0px 0px 1px #86969C');
-      }
-
-      if (scrollTop < (totalHeight / 2) - 200) {
-        $('#page-wrapper a').css('color', '#C7BFBF');
-      } else {
-        $('#page-wrapper a').css('color', '#6166FF');
-      }
-      if (scrollTop < (totalHeight / 2) - 200) {
-        $('h2, h3, h4, h5, h6, .h2, .h3, .h4, .h5, .h6').css('color', '#C7BFBF');
-      } else {
-        $('h2, h3, h4, h5, h6, .h2, .h3, .h4, .h5, .h6').css('color', '#593E22');
-      }
-    }
-
     $(window).scroll(function() {
-      bannerSize(heroFontSize);
+      if (album != true) {
+        bannerSize(heroFontSize);
+      }
+
       textColor();
     });
   bannerSize(heroFontSize);
